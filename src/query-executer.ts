df1ee60f9e4564f7d1df2a20f4d2bba575784df5
Muhammad Magdi <muhammad.magdi@emushrif.om>
2024-01-12 07:53:38 +0200

    add query-executer class to manage pool connections

---
+++ src/query-executer.ts
@@ -0,0 +1,21 @@
+import { Client, QueryResultRow } from "pg";
+import Pool from "pg-pool";
+
+export class QueryExecuter {
+  constructor(protected readonly pool: Pool<Client>) {}
+
+  protected async executeQuery<T extends QueryResultRow>(
+    query: string,
+    params?: any[]
+  ) {
+    const client = await this.pool.connect();
+
+    try {
+      return await client.query<T>(query, params);
+    } catch (error) {
+      throw error;
+    } finally {
+      client.release();
+    }
+  }
+}
--- src/queue-manager/index.ts
+++ src/queue-manager/index.ts
@@ -2,38 +2,41 @@ import { Client } from "pg";
 import Pool from "pg-pool";
 import { parseDbQueue } from "./helpers";
 import { Queue } from "./types";
+import { QueryExecuter } from "src/query-executer";
 
-export class QueueManager {
-  constructor(private readonly pool: Pool<Client>) {}
+export class QueueManager extends QueryExecuter {
+  constructor(pool: Pool<Client>) {
+    super(pool);
+  }
 
   public async list(): Promise<Queue[]> {
     const query = "SELECT pgmq.list_queues()";
-    const { rows } = await this.pool.query<{ list_queues: string }>(query);
+    const { rows } = await this.executeQuery<{ list_queues: string }>(query);
     return rows.map(({ list_queues }) => parseDbQueue(list_queues));
   }
 
   public async create(name: string) {
     const query = "SELECT pgmq.create($1)";
-    await this.pool.query(query, [name]);
+    await this.executeQuery(query, [name]);
   }
 
   public async createUnlogged(name: string) {
     const query = "SELECT pgmq.create_unlogged($1)";
-    await this.pool.query(query, [name]);
+    await this.executeQuery(query, [name]);
   }
 
   public async drop(name: string) {
     const query = "SELECT pgmq.drop_queue($1)";
-    await this.pool.query(query, [name]);
+    await this.executeQuery(query, [name]);
   }
 
   public async purge(name: string) {
     const query = "SELECT pgmq.purge_queue($1)";
-    await this.pool.query(query, [name]);
+    await this.executeQuery(query, [name]);
   }
 
   public async detachArchive(name: string) {
     const query = "SELECT pgmq.detach_archive($1)";
-    await this.pool.query(query, [name]);
+    await this.executeQuery(query, [name]);
   }
 }
